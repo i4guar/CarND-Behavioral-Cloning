50f9e89de58e4e21e05b1a121eefb55e77988496
i4guar <i4guar@gmail.com>
2020-04-10 14:56:59 +0000

    ADD visualization of model architecture

---
+++ model_vis.py
@@ -0,0 +1,8 @@
+from keras.models import load_model
+from keras.utils.vis_utils import plot_model
+
+modelname = 'model.h5'
+
+model = load_model(modelname)
+print(model.summary())
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
